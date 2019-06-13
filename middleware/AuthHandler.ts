d5f7340973307cece2c54af8b411d1838ba94b46
dark_shot <ameh46870@gmail.com>
2019-06-13 19:35:16 +0430

    better auth and error handling

--- middleware/AuthHandler.ts
+++ middleware/AuthHandler.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express'
 import jwt from 'jsonwebtoken'
 import db from '../models'
 import {ErrorHandler} from '../components/response'
-
+import _ from 'lodash'
 export default class AuthHandler{
     private static adminKey:string="Moz" + (process.env.APP_KEY || 'secret') + "Lorem Admin";
     public static async AdminVerify(token:string){
@@ -14,7 +14,8 @@ export default class AuthHandler{
             //@ts-ignore
             let admin = await db.Admin.findOne({where: {id: decode.data.id}, raw: true});
             if (admin) {
-                return admin;
+                //@ts-ignore
+                return decode.data;
             }
             let err=new ErrorHandler("Unauthorized user!",401,"UnAUTH");
             throw err;
@@ -22,5 +23,22 @@ export default class AuthHandler{
             let err=new ErrorHandler("Unauthorized user!",401,"UnAUTH");
             throw err;
         }
+    }
+    public static async AdminGen(admin:any){
+        if(admin&&_.isObject(admin)){
+            //@ts-ignore
+            if(admin.password){
+                //@ts-ignore
+                delete admin.password
+            }
+            //@ts-ignore
+            if(admin.reset_token){
+                //@ts-ignore
+                delete admin.reset_token
+            }
+        }
+        console.log(admin);
+        
+        return await jwt.sign({data: admin.dataValues}, this.adminKey)
     }
 }
